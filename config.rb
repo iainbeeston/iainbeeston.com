2e86ba241b3fd7dbbd91ae85724016e7bd98da42
Iain Beeston <iain.beeston@gmail.com>
2013-07-21 20:16:13 +1000

    Ignored source images (used to generate sprites) from the build

--- config.rb
+++ config.rb
@@ -53,6 +53,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+ignore 'images/sprites/icons/*'
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
--- extensions/sprite_factory.rb
+++ extensions/sprite_factory.rb
@@ -3,9 +3,10 @@ require 'sprite_factory'
 class SpriteFactoryExtension < Middleman::Extension
   def initialize(app, options_hash={}, &block)
     super
+    # TODO extract this to a proper option
+    options_style = 'scss'
+    # TODO automatically ignore (from the build) source files used to generate sprites
     app.before do
-      # TODO extract this to a proper option
-      options_style = 'scss'
       Dir.glob(File.join(app.source, app.images_dir, 'sprites', '*')).select{|f| File.directory?(f)}.each do |image_path|
         sprite_name = File.basename(image_path).chomp(File::SEPARATOR)
         style_path = File.join(app.source, app.css_dir, 'sprites', sprite_name + (options_style == "css" ? ".css" : ".css.#{options_style}"))
